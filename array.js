da4a4098df771f8f16f8a1bda03713ff139c6c3f
Fahimatul <ifahima92@gmail.com>
2022-08-09 18:11:27 +0600

    updated

---
+++ array.js
@@ -0,0 +1,16 @@
+var friendAge = [23, 24, 56, 90];
+var naikas = ["bobita", "opu", "mahi"];
+console.log(naikas);
+// console.log(naikas.length); -->length shows the number of the element
+
+// get element value by index
+var element = naikas[2];
+console.log(element);
+
+//set element value by index
+naikas[2] = "kajol";
+console.log(naikas);
+
+// find index(position) of an element
+var position = naikas.indexOf("opu");
+console.log(position);
---
+++ concatenation.js
@@ -0,0 +1,17 @@
+var firstName = 'fahimatul';
+var lastName = 'islam';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// convert string to integer number
+price1 = '31';
+price1New = parseInt(price1);
+console.log(price1New);
+
+// fix decimal number
+var num1 = .2;
+var num2 = .3;
+var sum = num1 + num2;
+sum = sum.toFixed(3);
+sum = parseFloat(sum);
+console.log(sum);
